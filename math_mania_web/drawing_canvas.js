4b53cfaf9658bb47d525f920453dbd2e0f61489a
Shimanto Bhowmik <bmikshimanto@gmail.com>
2022-07-06 12:54:12 +0600

    Added responsiveness for the canvas to draw the desired numbers by mouse clicks

--- math_mania_web/drawing_canvas.js
+++ math_mania_web/drawing_canvas.js
@@ -8,9 +8,12 @@ var currY = 0;
 var prevX = 0;
 var prevY = 0;
 
+var canvas;
+var context;
+
 function prepareCanvas () {
-    const canvas = document.getElementById('canvas');
-    const context = canvas.getContext('2d');
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
 
     context.fillStyle = BACKGROUND_COLOR;
     context.fillRect(0 , 0 , canvas.clientWidth, canvas.clientHeight)
@@ -19,7 +22,22 @@ function prepareCanvas () {
     context.lineWidth = LINE_WIDTH;
     context.lineJoin = 'round';
 
+    var isPainting = false;
+
+    document.addEventListener('mousedown', function(event){
+        console.log('Mouse Pressed');
+        isPainting = true;  
+        currX = event.clientX - canvas.offsetLeft;
+        currY = event.clientY - canvas.offsetTop;   
+    });
+
+    document.addEventListener('mouseup', function(event){
+        console.log('Mouse Released');
+        isPainting = false;  
+    });
+
     document.addEventListener('mousemove', function(event){
+        if (isPainting){
         prevX = currX;
         currX = event.clientX - canvas.offsetLeft;
 
@@ -31,6 +49,25 @@ function prepareCanvas () {
         context.lineTo(currX, currY);
         context.closePath();
         context.stroke();
+        }
 
     });
+
+    canvas.addEventListener('mouseleave', function(event){
+        isPainting = false;  
+    });
+
+    
+
+};
+
+function clearCanvas() {
+    currX = 0;
+    currY = 0;
+
+    prevX = 0;
+    prevY = 0;
+
+    context.fillRect(0 , 0 , canvas.clientWidth, canvas.clientHeight);
+
 };
